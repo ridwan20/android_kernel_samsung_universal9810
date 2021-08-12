3d91b51523499edc42e4efa72848eda409f1dad6
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2021-08-12 22:24:09 +0300

    BuildScript: add second zip support

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,6 +4,7 @@ export CROSS_COMPILE_ARM32=/home/$USER/Android/Toolchains/arm-eabi/bin/arm-eabi-
 export CC=/home/$USER/Android/Toolchains/clang/bin/clang
 
 ZIP_DIR="/home/$USER/Android/Kernel/Zip/"
+ZIP_ALT_DIR="/home/$USER/Android/Kernel/Zip_Alt/"
 CUR_DIR=$PWD
 
 export ANDROID_MAJOR_VERSION=r
@@ -19,6 +20,16 @@ function clean {
 
 all="false"
 clean="false"
+
+if [ -d $ZIP_ALT_DIR ] 
+then
+    printf "Alternative Zip Exists\n" 
+fi
+
+if [ ! -d $ZIP_DIR ] 
+then
+    printf "Zip Folder DOESN'T Exists." 
+fi
 
 while getopts ":ca" flag; do
   case "${flag}" in
@@ -42,12 +53,34 @@ then
 	make -j$(nproc --all)
 	cp -vr $CUR_DIR/arch/arm64/boot/Image $ZIP_DIR/Kernel/G960zImage
 	cp -vr $CUR_DIR/arch/arm64/boot/dtb.img $ZIP_DIR/Kernel/G960dtb.img
+
+if [ -d $ZIP_ALT_DIR ] 
+then
+    printf "Building G960 Alternative\n"
+	make exynos9810-starlte_defconfig
+	sed -i 's/CONFIG_SECURITY_SELINUX_NEVER_ENFORCE=y/# CONFIG_SECURITY_SELINUX_NEVER_ENFORCE is not set/g' "$CUR_DIR"/.config
+	make -j$(nproc --all)
+	cp -vr $CUR_DIR/arch/arm64/boot/Image $ZIP_ALT_DIR/Kernel/G960zImage
+	cp -vr $CUR_DIR/arch/arm64/boot/dtb.img $ZIP_ALT_DIR/Kernel/G960dtb.img
+fi
+	
 	clean
 	printf "Building N960\n"
 	make exynos9810-crownlte_defconfig
 	make -j$(nproc --all)
 	cp -vr $CUR_DIR/arch/arm64/boot/Image $ZIP_DIR/Kernel/N960zImage
 	cp -vr $CUR_DIR/arch/arm64/boot/dtb.img $ZIP_DIR/Kernel/N960dtb.img
+	
+if [ -d $ZIP_ALT_DIR ] 
+then
+    printf "Building N960 Alternative\n"
+	make exynos9810-crownlte_defconfig
+	sed -i 's/CONFIG_SECURITY_SELINUX_NEVER_ENFORCE=y/# CONFIG_SECURITY_SELINUX_NEVER_ENFORCE is not set/g' "$CUR_DIR"/.config
+	make -j$(nproc --all)
+	cp -vr $CUR_DIR/arch/arm64/boot/Image $ZIP_ALT_DIR/Kernel/N960zImage
+	cp -vr $CUR_DIR/arch/arm64/boot/dtb.img $ZIP_ALT_DIR/Kernel/N960dtb.img
+fi
+	
 	clean
 fi
 printf "Building G965\n"
@@ -55,3 +88,13 @@ make exynos9810-star2lte_defconfig
 make -j$(nproc --all)
 cp -vr $CUR_DIR/arch/arm64/boot/Image $ZIP_DIR/Kernel/G965zImage
 cp -vr $CUR_DIR/arch/arm64/boot/dtb.img $ZIP_DIR/Kernel/G965dtb.img
+
+if [ -d $ZIP_ALT_DIR ] 
+then
+printf "Building G965 Alternative\n"
+make exynos9810-star2lte_defconfig
+sed -i 's/CONFIG_SECURITY_SELINUX_NEVER_ENFORCE=y/# CONFIG_SECURITY_SELINUX_NEVER_ENFORCE is not set/g' "$CUR_DIR"/.config
+make -j$(nproc --all)
+cp -vr $CUR_DIR/arch/arm64/boot/Image $ZIP_ALT_DIR/Kernel/G965zImage
+cp -vr $CUR_DIR/arch/arm64/boot/dtb.img $ZIP_ALT_DIR/Kernel/G965dtb.img
+fi
